847009691c820d14b8727f5bf9d81e2d4eb3c034
Lokesh-Rana-lab <lokeshranarana13@gmail.com>
2026-07-16 09:16:12 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (35.01%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -14,16 +14,30 @@
  * }
  */
 class Solution {
-    public TreeNode searchBST(TreeNode root, int key) {
-        TreeNode cur = root;
-        while(cur!=null && cur.val!=key){
-            if(cur.val>key){
 
-                cur=cur.left;
-            }
-            else 
-            cur=cur.right;
+    TreeNode rec(TreeNode node, int key){
+         if(node==null) return null;
+        if(node.val==key) return node ;
+        if(node.val>key){
+            return rec(node.left,key);
         }
-        return cur;
+        else return  rec(node.right,key);
     }
+    public TreeNode searchBST(TreeNode root, int key) {
+       
+
+        // TreeNode cur = root;
+        // while(cur!=null && cur.val!=key){
+        //     if(cur.val>key){
+
+        //         cur=cur.left;
+        //     }
+        //     else 
+        //     cur=cur.right;
+        // }
+        // return cur;
+
+       return rec(root,key);
+    }
+
 }
