01417664f48002efee8ba6a1cf08b38f82349b44
Lokesh-Rana-lab <lokeshranarana13@gmail.com>
2026-07-20 23:05:08 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (79.82%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,24 @@
+class Solution {
+public int removeElement(int[] nums, int val) {
+// int k = 0;
+// two pointer
+// for (int i = 0; i < nums.length; i++) {
+//     if (nums[i] != val) {
+//         nums[k] = nums[i];
+//         k++;
+//     }
+// }
+
+// return k;  
+int left = 0, right = nums.length - 1;
+while (left <= right) {
+if (nums[left] == val) {
+nums[left] = nums[right--];
+} else {
+left++;
+}
+}
+return left;
+
+}
+}
