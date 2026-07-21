b20749d05d8765a15963de1247ca093f0adaedaf
Lokesh-Rana-lab <lokeshranarana13@gmail.com>
2026-07-21 17:53:47 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (85.84%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -13,18 +13,38 @@
  *     }
  * }
  */
+
+
+//  class Solution {
+//     List<Integer> traverseRec(TreeNode root,List<Integer> list){
+//         if(root == null) return list;
+//         list.add(root.val);
+//         traverseRec(root.left,list);
+//         traverseRec(root.right,list);
+//         return list;
+//     }
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         List <Integer> list = new ArrayList<>();
+//         list =  traverseRec(root,list);
+//         return list;
+
+//     }
+// }
 class Solution {
-    List<Integer> traverseRec(TreeNode root,List<Integer> list){
-        if(root == null) return list;
-        list.add(root.val);
-        traverseRec(root.left,list);
-        traverseRec(root.right,list);
-        return list;
+    public void dfs(TreeNode root,List<Integer> ans) {
+
+     if(root == null) return;
+     ans.add(root.val);
+     dfs(root.left,ans);
+     dfs(root.right,ans);
+        
     }
     public List<Integer> preorderTraversal(TreeNode root) {
-        List <Integer> list = new ArrayList<>();
-        list =  traverseRec(root,list);
-        return list;
+        ArrayList<Integer> ans = new ArrayList<>();
+        dfs(root,ans);
+        return ans;
 
+        
+        
     }
 }
