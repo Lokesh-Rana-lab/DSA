c18c24fcddc86174863cbe4dca384d25334b95c4
Lokesh-Rana-lab <lokeshranarana13@gmail.com>
2026-07-28 01:31:08 +0530

    Time: 1 ms (31.61%) | Memory: 45.7 MB (41.74%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -13,16 +13,36 @@
  *     }
  * }
  */
+// class Solution {
+//     public boolean isBalanced(TreeNode root) {
+//         if(root == null) return true;
+//         int leftlv = levels(root.left);
+//         int rightlv = levels(root.right);
+//         if(Math.abs(leftlv-rightlv)>1) return false;
+//         return isBalanced(root.left) && isBalanced(root.right);
+//     }
+//     private int levels(TreeNode root){
+//         if(root==null) return 0;
+//         return 1+ Math.max(levels(root.left),levels(root.right));
+//     }
+// }
+
+
+
+
 class Solution {
-    public boolean isBalanced(TreeNode root) {
+    static boolean flag;
+    public boolean isBalanced(TreeNode root) {    // optimise
         if(root == null) return true;
-        int leftlv = levels(root.left);
-        int rightlv = levels(root.right);
-        if(Math.abs(leftlv-rightlv)>1) return false;
-        return isBalanced(root.left) && isBalanced(root.right);
+        flag = true;
+        levels(root);
+        return flag;
     }
     private int levels(TreeNode root){
         if(root==null) return 0;
-        return 1+ Math.max(levels(root.left),levels(root.right));
+        int leftlv= levels(root.left);
+        int rightlv=levels(root.right);
+        if(Math.abs(leftlv-rightlv)>1) flag = false;
+        return 1+ Math.max(leftlv,rightlv);
     }
 }
