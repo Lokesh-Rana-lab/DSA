37e32167b9ea071beb11d5aa113b64a78804134a
Lokesh-Rana-lab <lokeshranarana13@gmail.com>
2026-08-03 16:02:28 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (82.59%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -14,16 +14,31 @@
  * }
  */
 class Solution {
-    public void inorder(TreeNode root, ArrayList<Integer> arr ){
+    // public void inorder(TreeNode root, ArrayList<Integer> arr ){
+    //     if(root == null) return;
+    //     inorder(root.left , arr);
+    //     arr.add(root.val);
+    //     inorder(root.right,arr);
+    // }
+    // public int kthSmallest(TreeNode root, int k) {
+    //     ArrayList<Integer> arr = new ArrayList<>();
+    //     inorder(root,arr);
+    //     if(arr.size()<k) return -1;
+    //     return arr.get(k-1);
+
+     static int ans;
+     static int k2;
+    public void inorder(TreeNode root){
         if(root == null) return;
-        inorder(root.left , arr);
-        arr.add(root.val);
-        inorder(root.right,arr);
+        inorder(root.left);
+        k2--;
+        if(k2==0) ans=root.val;
+        inorder(root.right);
     }
     public int kthSmallest(TreeNode root, int k) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        inorder(root,arr);
-        if(arr.size()<k) return -1;
-        return arr.get(k-1);
+         ans =-1;
+         k2=k;
+        inorder(root);
+        return ans;
     }
 }
